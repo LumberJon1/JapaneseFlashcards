d8a6b602dd3ebcd2981557136ace7210605df86a
Jonathan W <jwilliams12117@gmail.com>
2022-12-09 16:17:39 -0700

    Added conditionals and formatted string output

--- time.js
+++ time.js
@@ -4,15 +4,15 @@
 // Define arrays to store Japanese number values
 const hours = ["ichi", "ni", "san", "yo", "go", "roku", "shichi", "hachi", "ku", "juu", "juu-ichi", "juu-ni"];
 const tens = ["ichi", "ni", "san", "yon", "go"];
-const minutes = ["ippun", "ni-fun", "san-pun", "yon-pun", "go-fun", "roppun", "nana-fun", "happun", "kyuu-fun"]; // Needs an edge case for ":10"
+const minutes = ["ippun", "ni-fun", "san-pun", "yon-pun", "go-fun", "roppun", "nana-fun", "happun", "kyuu-fun"];
 
 
 // Generate test time
-let testTime = "11:10";
+let testTime = "10:48";
 
 // Split time into hh and mm components
 let timeArray = (""+testTime).split(":");
-console.log(timeArray);
+
 let hh = parseInt(timeArray[0]);
 let mm = parseInt(timeArray[1]);
 
@@ -28,13 +28,27 @@ outputArray.push(hours[hh-1]+"-ji");
 if (mm === 10) {
     outputArray.push("juppun");
 }
-else {
-    // split mm into tens and ones
-    // For the tens, access the numbers array at the index and add "-juu"
-    
-
-    // For ones, use switch case to add unique variants.
-
+else if (mm === 30) {
+    outputArray.push("han");
 }
+else {
+    // Create intermediary array to hold the recombined minutes for joining in final output
+    let finalMinutes = [];
+
+    // split mm into tens and ones
+    mm = (""+mm).split("");
+
+    // For the tens, access the numbers array at the index and add "-juu"
+    finalMinutes.push(tens[mm[0]-1]+"-juu");
+
+    // For ones, use the proper index from the minues constant
+    finalMinutes.push(minutes[mm[1]-1]);
+
+    finalMinutes = finalMinutes.join(" ");
+    outputArray.push(finalMinutes);
+}
+
+// Join hh and mm with a whitespace
+outputArray = outputArray.join(" ");
 
 console.log("\nOutput: "+outputArray);
