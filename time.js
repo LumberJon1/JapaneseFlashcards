5b72166effd6f21be83ce0af289c90fe16b0d8c1
Jonathan W <jwilliams12117@gmail.com>
2022-12-09 16:44:52 -0700

    Introduced random timestamps and fixed some errors that occurred as a result

--- time.js
+++ time.js
@@ -7,8 +7,23 @@ const tens = ["ichi", "ni", "san", "yon", "go"];
 const minutes = ["ippun", "ni-fun", "san-pun", "yon-pun", "go-fun", "roppun", "nana-fun", "happun", "kyuu-fun"];
 
 
+// Generate a random timestamp
+let timestampHours = Math.floor(Math.random() * 11) + 1;
+let timestampMinutes = Math.floor(Math.random() * 61);
+
+// Account for single-digit random numbers by adding a 0
+if (timestampHours < 10) {
+    timestampHours = ("0"+timestampHours);
+}
+if (timestampMinutes < 10) {
+    timestampMinutes = ("0"+timestampMinutes);
+}
+
 // Generate test time
-let testTime = "10:48";
+// let testTime = "10:48";
+let testTime = (timestampHours+":"+timestampMinutes);
+
+console.log("\ntestTime: "+testTime);
 
 // Split time into hh and mm components
 let timeArray = (""+testTime).split(":");
@@ -39,11 +54,23 @@ else {
     mm = (""+mm).split("");
 
     // For the tens, access the numbers array at the index and add "-juu"
-    finalMinutes.push(tens[mm[0]-1]+"-juu");
+    console.log("mm: "+mm);
+    console.log("mm[0]: "+mm[0])
+    if (mm[0] === "1") {
+        finalMinutes.push("juu")
+    }
+    else if (mm[0] === "0") {
+        console.log("was undefined");
+        finalMinutes.push("");
+    }
+    else {
+        finalMinutes.push(tens[mm[0]-1]+"-juu");
+    }
 
     // For ones, use the proper index from the minues constant
     finalMinutes.push(minutes[mm[1]-1]);
 
+    // Join and format the mm section to then push to final output and return
     finalMinutes = finalMinutes.join(" ");
     outputArray.push(finalMinutes);
 }
